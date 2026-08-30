b7eda387f34a521b494f6aa26a9d42b1403dfc0e
Jugan65 <139189914+Jugan65@users.noreply.github.com>
2026-08-30 18:19:16 +0800

    Create engine_interface.py

---
+++ interface/engine_interface.py
@@ -0,0 +1,12 @@
+"""
+Public interface for 64D-DRE replication package.
+"""
+import core_engine
+
+DREngine = core_engine.DREngine
+State = core_engine.State
+single_bit_flip = core_engine.single_bit_flip
+global_inversion = core_engine.global_inversion
+bit_reversal_permutation = core_engine.bit_reversal_permutation
+middle_reconstruction = core_engine.middle_reconstruction
+find_path = core_engine.find_path
